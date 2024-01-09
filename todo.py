268240f6478885dd17356c22b005d762f9b8b8c8
kasloww <kdavidson@brooklynsteamcenter.org>
2024-01-09 10:46:22 -0500

    finally its done :D

--- todo.py
+++ todo.py
@@ -4,30 +4,40 @@ import pymysql.cursors
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == "POST":
-        new_todo = request.form["new_todo"]
-        todos.append(new_todo) 
-    return render_template('first.html.jinja', my_todos=todos)
 conn = pymysql.connect(
     database="kdavidson_todos",
     user="kdavidson",
     password="231534561",
     host="10.100.33.60",
     cursorclass=pymysql.cursors.DictCursor
-    )
+)
 
-cursor = conn.cursor()
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == "POST":
+        new_todo = request.form["new_todo"]
+        todos.append(new_todo) 
+        cursor = conn.cursor()
+        cursor.execute(f"INSERT INTO `todos` (`description`) VALUES ('{new_todo}')")
+        cursor.close()
+        conn.commit()
 
-cursor.execute("SELECT `description` FROM `todos`")
+    cursor = conn.cursor()
 
-results = cursor.fetchall()
+    cursor.execute("SELECT * from `todos`")
+
+    results = cursor.fetchall()
+
+    cursor.close()
+    return render_template('first.html.jinja', my_todos=results)
+
 
 todos = ['have fun', 'figure myself out', 'thank god for another year']
 
 @app.route("/delete_todo/<int:todo_index>", methods=["POST"])
 def todo_delete(todo_index):
-    del todos[todo_index]
-
+    cursor = conn.cursor()
+    cursor.execute(f"DELETE FROM `todos` WHERE `id` = {todo_index}")
+    cursor.close()
+    conn.commit()
     return redirect('/')
